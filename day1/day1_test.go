3f403c851a562ababd5f6038bc62bbf5fe380f89
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2022-12-02 18:18:13 +0100

    add result tests too

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -8,9 +8,14 @@ import (
 
 func TestDay1Part1(t *testing.T) {
 	expect := 24000
-
   got := Day1Part1(reader.CreateReaderFromFile("./example.txt"))
+  if got != expect {
+    log.Println(got)
+    t.Fail()
+  }
 
+	expect = 69912
+  got = Day1Part1(reader.CreateReaderFromFile("./input.txt"))
   if got != expect {
     log.Println(got)
     t.Fail()
@@ -19,9 +24,14 @@ func TestDay1Part1(t *testing.T) {
 
 func TestDay1Part2(t *testing.T) {
 	expect := 45000
-
   got := Day1Part2(reader.CreateReaderFromFile("./example.txt"))
+  if got != expect {
+    log.Println(got)
+    t.Fail()
+  }
 
+	expect = 208180
+  got = Day1Part2(reader.CreateReaderFromFile("./input.txt"))
   if got != expect {
     log.Println(got)
     t.Fail()
