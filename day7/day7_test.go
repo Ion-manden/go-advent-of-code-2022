85d6d2715eeb4a7c62fb69909ceee1d79ea8f2d4
Nicolai Thomassen <nicolai-thomassen@hotmail.com>
2022-12-11 15:29:12 +0100

    better test output for day 7

--- day7/day7_test.go
+++ day7/day7_test.go
@@ -18,7 +18,8 @@ func TestDay7Part1(t *testing.T) {
 	expect = 1367870
 	got = Day7Part1(reader.CreateReaderFromFile("./input.txt"))
 	if got != expect {
-		log.Println(got)
+		log.Println("got: ", got)
+		log.Println("expect: ", expect)
 		t.Fail()
 	}
 }
@@ -27,14 +28,16 @@ func TestDay7Part2(t *testing.T) {
 	expect := 24933642
 	got := Day7Part2(reader.CreateReaderFromFile("./example.txt"))
 	if got != expect {
-		log.Println(got)
+		log.Println("got: ", got)
+		log.Println("expect: ", expect)
 		t.Fail()
 	}
 
 	expect = 549173
 	got = Day7Part2(reader.CreateReaderFromFile("./input.txt"))
 	if got != expect {
-		log.Println(got)
+		log.Println("got: ", got)
+		log.Println("expect: ", expect)
 		t.Fail()
 	}
 }
